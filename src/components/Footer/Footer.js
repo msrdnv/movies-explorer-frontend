958e410957eddd69d66c229ecaec076df51b9d0a
msrdnv <msrdnv@ya.ru>
2023-10-29 21:19:07 +0300

    feat: complete footer 1280

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
 export default function Footer() {
   return (
-    <></>
+    <div className='footer'>
+      <p className='footer__caption'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__container'>
+        <p className='footer__text'>© 2020</p>
+        <nav className='footer__nav-container'>
+          <a className='footer__text' href='https://practicum.yandex.ru/'>Яндекс.Практикум</a>
+          <a className='footer__text' href='https://github.com/'>Github</a>
+        </nav>
+      </div>
+    </div>
   );
 }
