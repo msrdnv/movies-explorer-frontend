c9ae6b8d65616db21470c615a9ac61be482fd5d9
msrdnv <msrdnv@ya.ru>
2023-10-31 20:58:08 +0300

    feat: complete notfoundpage 1280

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,5 +1,16 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import './NotFoundPage.css'
+
 export default function NotFoundPage() {
+
+  const navigate = useNavigate();
+
   return (
-    <></>
+    <div className='not-found-page'>
+      <h2 className='not-found-page__title'>404</h2>
+      <p className='not-found-page__subtitle'>Страница не найдена</p>
+      <Link className='not-found-page__return-link' onClick={navigate(-1)}>Назад</Link>
+    </div>
   );
 }
