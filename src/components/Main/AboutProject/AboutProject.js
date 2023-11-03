8a6aaa2dc7731cb65781f1b0d22d5ac25fa56fbd
msrdnv <msrdnv@ya.ru>
2023-11-03 18:18:01 +0300

    feat: complete about project 768

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -5,18 +5,18 @@ export default function AboutProject() {
   return (
     <div className='about-project' id='about-project'>
       <h2 className='about-project__title'>О проекте</h2>
-      <div className='about-project__table'>
-        <h3 className='about-project__table-title'>Дипломный проект включал 5 этапов</h3>
-        <h3 className='about-project__table-title'>На выполнение диплома ушло 5 недель</h3>
-        <p className='about-project__table-caption'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-        <p className='about-project__table-caption'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-      </div>
-      <div className='about-project__scheme'>
-        <p className='about-project__scheme-element about-project__scheme-element_green'>1 неделя</p>
-        <p className='about-project__scheme-element about-project__scheme-element_grey'>4 недели</p>
-        <p className='about-project__scheme-element'>Back-end</p>
-        <p className='about-project__scheme-element'>Front-end</p>
-      </div>
+      <ul className='about-project__info'>
+        <li className='about-project__info-title'>Дипломный проект включал 5 этапов</li>
+        <li className='about-project__info-title'>На выполнение диплома ушло 5 недель</li>
+        <li className='about-project__info-caption'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</li>
+        <li className='about-project__info-caption'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</li>
+      </ul>
+      <ul className='about-project__graphic-data'>
+        <li className='about-project__graphic-data-element about-project__graphic-data-element_green'>1 неделя</li>
+        <li className='about-project__graphic-data-element about-project__graphic-data-element_grey'>4 недели</li>
+        <li className='about-project__graphic-data-element'>Back-end</li>
+        <li className='about-project__graphic-data-element'>Front-end</li>
+      </ul>
     </div>
   );
 }
