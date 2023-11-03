3998959144c02c2db9424d84a1db467ce30b37c0
msrdnv <msrdnv@ya.ru>
2023-11-03 18:56:35 +0300

    feat: complete techs 768

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -5,19 +5,19 @@ export default function Techs() {
   return (
     <div className='techs'>
       <h2 className='techs__title'>Технологии</h2>
-      <div className='techs__main-container'>
-        <h3 className='techs__main-title'>7 технологий</h3>
+      <div className='techs__text-container'>
+        <h3 className='techs__subtitle'>7 технологий</h3>
         <p className='techs__caption'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
       </div>
-      <div className='techs__blocks'>
-        <p className='techs__block'>HTML</p>
-        <p className='techs__block'>CSS</p>
-        <p className='techs__block'>JS</p>
-        <p className='techs__block'>React</p>
-        <p className='techs__block'>Git</p>
-        <p className='techs__block'>Express.js</p>
-        <p className='techs__block'>mongoDB</p>
-      </div>
+      <ul className='techs__blocks'>
+        <li className='techs__block'>HTML</li>
+        <li className='techs__block'>CSS</li>
+        <li className='techs__block'>JS</li>
+        <li className='techs__block'>React</li>
+        <li className='techs__block'>Git</li>
+        <li className='techs__block'>Express.js</li>
+        <li className='techs__block'>mongoDB</li>
+      </ul>
     </div>
   );
 }
