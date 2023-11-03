3b852fb116c47fa8b17400f46fab784e69986642
msrdnv <msrdnv@ya.ru>
2023-11-03 19:29:50 +0300

    feat: complete about me, portfolio 768

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -8,10 +8,10 @@ export default function AboutMe() {
       <h2 className='about-me__title'>Студент</h2>
       <div className='about-me__main-container'>
         <div className='about-me__info-container'>
-          <h3 className='about-me__name'>Виталий</h3>
-          <p className='about-me__about-caption'>Фронтенд-разработчик, 30 лет</p>
-          <p className='about-me__about-text'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена <br/>и дочь.
-          Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
+          <h3 className='about-me__subtitle'>Виталий</h3>
+          <p className='about-me__caption'>Фронтенд-разработчик, 30 лет</p>
+          <p className='about-me__text'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и&nbsp;дочь.
+          Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С&nbsp;2015 года работал в компании «СКБ Контур».
           После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
           <a className='about-me__git-link' href='https://github.com/msrdnv'>Github</a>
         </div>
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -5,18 +5,18 @@ export default function Portfolio() {
   return (
     <div className='portfolio'>
       <h2 className='portfolio__title'>Портфолио</h2>
-      <div className='portfolio__nav-block'>
-        <p className='portfolio__nav-caption'>Статичный сайт</p>
+      <nav className='portfolio__nav-block'>
+        <p className='portfolio__nav-link-name'>Статичный сайт</p>
         <a className='portfolio__nav-link' href='https://msrdnv.github.io/how-to-learn/'>↗</a>
-      </div>
-      <div className='portfolio__nav-block'>
-        <p className='portfolio__nav-caption'>Адаптивный сайт</p>
+      </nav>
+      <nav className='portfolio__nav-block'>
+        <p className='portfolio__nav-link-name'>Адаптивный сайт</p>
         <a className='portfolio__nav-link' href='https://msrdnv.github.io/russian-travel/'>↗</a>
-      </div>
-      <div className='portfolio__nav-block'>
-        <p className='portfolio__nav-caption'>Одностраничное приложение</p>
+      </nav>
+      <nav className='portfolio__nav-block'>
+        <p className='portfolio__nav-link-name'>Одностраничное приложение</p>
         <a className='portfolio__nav-link' href='https://mesto-frontend.msrdnv.nomoredomainsrocks.ru/'>↗</a>
-      </div>
+      </nav>
     </div>
   );
 }
